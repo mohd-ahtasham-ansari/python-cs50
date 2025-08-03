994198f99b8660bc824c97ef5441f7cd9934d4e3
mohd ahtasham ansari <aahtasham102@gmail.com>
2025-08-03 12:57:34 +0530

    grades programs

--- conditionals/grade.py
+++ conditionals/grade.py
@@ -1,3 +1,4 @@
+"""
 Score =int(input("what is your score ? "))
 if Score>=90 and Score<=100:
     print("Grade : A")
@@ -8,6 +9,21 @@ elif Score>=70 and Score<80:
 elif Score>=60 and Score<70:
     print("Grade : D")
 elif Score>=50 and Score<60:
+    print("Grade : E")
+else:
+    print("Grade : F")
+
+"""
+Score =int(input("what is your score ? "))
+if 90<= Score<=100:
+    print("Grade : A")
+elif 80<= Score<90:
+    print("Grade : B")
+elif  70<=Score<80:
+    print("Grade : C")
+elif 60<= Score<70:
+    print("Grade : D")
+elif 50<= Score<60:
     print("Grade : E")
 else:
     print("Grade : F")
