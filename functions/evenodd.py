8ffc517a98cc26f2f8dccaff0dceb05d5b4f729f
mohd ahtasham ansari <aahtasham102@gmail.com>
2025-08-03 13:37:42 +0530

    is even?

---
+++ functions/evenodd.py
@@ -0,0 +1,14 @@
+def main():
+    x = int(input("what is x? "))
+    if is_even(x):
+        print("the x is EVEN")
+    else:
+        print("the x is ODD")
+
+def is_even(n):
+    if n%2==0:
+        return True
+    else:
+        return False 
+
+main()
