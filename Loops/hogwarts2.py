9b9b854506a0068bf2586177ccf63034c94e302e
mohd ahtasham ansari <aahtasham102@gmail.com>
2025-08-03 15:29:29 +0530

    loops

---
+++ Loops/hogwarts2.py
@@ -0,0 +1,9 @@
+students={
+    "Hermoine":"Gryffindor",
+    "harry": "Gryffindor",
+    "ron" :"Gryffindor",
+    "draco":"Slytherin",
+}
+
+for student in students:
+    print(student,students[student],sep=", ")
