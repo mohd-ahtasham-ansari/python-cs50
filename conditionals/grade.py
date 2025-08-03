a1184213dd4d8024b325c711ead675a58e332d56
mohd ahtasham ansari <aahtasham102@gmail.com>
2025-08-03 12:48:27 +0530

    grade

---
+++ conditionals/grade.py
@@ -0,0 +1,13 @@
+Score =int(input("what is your score ? "))
+if Score>=90 and Score<=100:
+    print("Grade : A")
+elif Score>=80 and Score<90:
+    print("Grade : B")
+elif Score>=70 and Score<80:
+    print("Grade : C")
+elif Score>=60 and Score<70:
+    print("Grade : D")
+elif Score>=50 and Score<60:
+    print("Grade : E")
+else:
+    print("Grade : F")
