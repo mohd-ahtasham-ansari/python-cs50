eb8cb2dd66acef31bd25ab5f22915541d75a5169
mohd ahtasham ansari <aahtasham102@gmail.com>
2025-08-03 14:12:24 +0530

    harrypotter house

---
+++ conditionals/house.py
@@ -0,0 +1,8 @@
+name = input("whats yous name?  ")
+
+if name=="harry" or name=="hermione" or name=="ron":
+    print("Gryffindor")
+elif name=="draco":
+    print("Slytherin")
+else:
+    print("who? ")
