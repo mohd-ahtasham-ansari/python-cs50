650974f722a998e5db7e9d5ba64a41ec2c90d2d6
mohd ahtasham ansari <aahtasham102@gmail.com>
2025-08-03 14:01:57 +0530

    is even

--- functions/evenodd.py
+++ functions/evenodd.py
@@ -4,11 +4,15 @@ def main():
         print("the x is EVEN")
     else:
         print("the x is ODD")
-        
+"""      
 def is_even(n):
     if n%2==0:
         return True
     else:
         return False 
+"""
+
+def is_even(n):
+    return True if n%2==0 else False
 
 main()
